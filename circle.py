a1420bd42a34b28123a8481af68bd1980257a62e
Vorfreu <ugurxay@gmail.com>
2021-10-08 16:30:16 +0300

    update

--- circle.py
+++ circle.py
@@ -1,9 +1,9 @@
 from manim import *
 
 class TwoCircles(Scene):
-    def construct(self):
-        axes = ComplexPlane().add_coordinates()
 
+    def construct(self):
+        axes = ComplexPlane()
 
         circle1 = Circle(radius=1.4,color=RED)
         circle1.shift(RIGHT*2)
@@ -17,12 +17,20 @@ class TwoCircles(Scene):
         circle4 = Circle(radius=5,color=BLUE)
         circle4.shift(UP*2)
 
-        line1 = Line(start=([-3,-3,0]),end=([3,3,0]))
+        dot1 = Dot([1,1,0],color=PURPLE)
+        dot2 = Dot([-2.45,-2.45,0])
+        dot3 = Dot([4.45,4.45,0])
 
-        self.add(axes,line1)
-        self.play(Create(circle1))  # show the circle on screen
+        line1 = Line(start=dot1,end=dot3,color=PURPLE_A)
+        line2 = Line(start=dot1,end=dot2,color=PURPLE_B)
+        self.play(Create(axes), run_time=3)
+        self.play(Create(circle1))
         self.wait(0.5)
         self.play(Create(circle2))
         self.wait(1)
-        self.play(Transform(circle1,circle3),Transform(circle2,circle4), run_time=6, rate_func=there_and_back_with_pause)
+        self.play(Create(dot1))
+        self.wait(0.5)
+        self.play(Transform(circle1,circle3),Transform(circle2,circle4),Create(line1),Create(line2), run_time=6, rate_func=there_and_back_with_pause)
         self.wait(1)
+
+
